f67454b7a5f4896e1d23a7823df09a9896918d82
Andrey Brus <brus.andrey@gmail.com>
2015-08-24 22:52:08 +0300

    First clear code

--- SwingGuiTutorial/src/MainApp.java
+++ SwingGuiTutorial/src/MainApp.java
@@ -1,7 +1,6 @@
 import javax.swing.JFrame;
 
 public class MainApp {
-// set some another changes 2
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
