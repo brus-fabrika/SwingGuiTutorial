3bb79f2bc8321c45431fc032bc80359668bcebae
Andrey Brus <andrey.brus@revimedia.com>
2015-08-28 14:00:57 +0300

    Added size, location, title and close operation

--- SwingGuiTutorial/src/MainApp.java
+++ SwingGuiTutorial/src/MainApp.java
@@ -1,10 +1,15 @@
 import javax.swing.JFrame;
 
 public class MainApp {
-// set some another changes
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
+		
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		
+		frame.setTitle("My first Swing application");
+		frame.setSize(300, 300);
+		frame.setLocation(500, 500);
+		
 		frame.setVisible(true);
 	}
-
 }
