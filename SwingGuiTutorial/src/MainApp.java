64d047243971cdffa14b45362162ff192bfa8008
Andrey Brus <brus.andrey@gmail.com>
2015-08-28 19:13:42 +0300

    Design ready

--- SwingGuiTutorial/src/MainApp.java
+++ SwingGuiTutorial/src/MainApp.java
@@ -1,8 +1,4 @@
-import java.awt.BorderLayout;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 public class MainApp {
@@ -19,17 +15,8 @@ public class MainApp {
 	}
 	
 	public static void createGui() {
-		JFrame frame = new JFrame();
-		
+		JFrame frame = new MainWindow();
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
-		frame.setTitle("My first Swing application");
-		frame.setSize(300, 300);
-		frame.setLocation(500, 500);
-		
-		frame.add(new JLabel("test"), BorderLayout.WEST);
-		frame.add(new JButton("OK"), BorderLayout.NORTH);
-		
 		frame.setVisible(true);
 	}
 }
--- SwingGuiTutorial/src/MainWindow.java
+++ SwingGuiTutorial/src/MainWindow.java
@@ -1,8 +1,11 @@
-import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
 
 public class MainWindow extends JFrame{
@@ -15,7 +18,23 @@ public class MainWindow extends JFrame{
 		setSize(300, 300);
 		setLocation(500, 500);
 		
-		add(new JLabel("test"), BorderLayout.WEST);
-		add(new JButton("OK"), BorderLayout.NORTH);
+		JPanel panel = new JPanel();
+		panel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		GridLayout layout = new GridLayout(3, 2);
+		layout.setHgap(10);
+		layout.setVgap(10);
+		
+		panel.setLayout(layout);
+		panel.add(new JLabel("Celsium:"));
+		panel.add(new JTextField("0.0"));
+		panel.add(new JLabel("Fahrenheit:"));
+		panel.add(new JTextField("0.0"));
+		panel.add(new JButton("OK"));
+		panel.add(new JButton("Cancel"));
+		
+		add(panel);
+		
+		pack();
 	}
 }
