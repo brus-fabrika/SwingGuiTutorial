7e795047899ea279ae946267c1fc5ecbb20fbae1
Andrey Brus <andrey.brus@revimedia.com>
2015-08-28 14:14:43 +0300

    Added invoke later (with inner Runnable class :) )

--- SwingGuiTutorial/src/MainApp.java
+++ SwingGuiTutorial/src/MainApp.java
@@ -1,7 +1,20 @@
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class MainApp {
+	
+	static class GuiRunner implements Runnable {
+		@Override
+		public void run() {
+			createGui();
+		}
+	}
+	
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new GuiRunner());
+	}
+	
+	public static void createGui() {
 		JFrame frame = new JFrame();
 		
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
