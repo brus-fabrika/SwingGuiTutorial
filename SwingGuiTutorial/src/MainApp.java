570732bf4c28ae3ef9f7c06a5c27d21c0ec82670
Andrey Brus <andrey.brus@revimedia.com>
2016-02-23 17:54:51 +0200

    fixes after merge

--- SwingGuiTutorial/src/MainApp.java
+++ SwingGuiTutorial/src/MainApp.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
@@ -38,27 +37,3 @@ public class MainApp {
 		f.startMoving();
 	}
 }
-=======
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
-
-public class MainApp {
-	
-	static class GuiRunner implements Runnable {
-		@Override
-		public void run() {
-			createGui();
-		}
-	}
-	
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(new GuiRunner());
-	}
-	
-	public static void createGui() {
-		JFrame frame = new MainWindow();
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		frame.setVisible(true);
-	}
-}
->>>>>>> branch 'master' of https://github.com/brus-fabrika/SwingGuiTutorial.git
